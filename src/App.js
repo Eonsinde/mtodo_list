478932fcaa8a888c40752bbb4609a7b26d348963
olasinde <olasinde>
2021-02-01 02:47:52 +0100

    installed new packages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    // axios.get('http://127.0.0.1:8000/api/todos/')
     axios.get("https://jsonplaceholder.typicode.com/todos")
       .then(res => res.data)
       .then(TODOS => this.setState({todos: TODOS}))
